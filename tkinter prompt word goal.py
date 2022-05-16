ae75ed2f39de194f487c98ffc8f404282fbeaa8c
Ken <saburiken01@gmail.com>
2022-05-16 21:58:28 +0300

    Ken parts

---
+++ tkinter prompt word goal.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import *
+import datetime
+
+window = tk.Tk()
+window.geometry('320x150+500+85')
+window.title("Character text target")
+
+#exporting entered data
+def save():
+    now = datetime.datetime.now()
+    #save data in txt file
+
+    s= entry.get()
+    f = open(("texttarget"+entry.get()+".txt"), "w")
+    f.write(s)
+    f.close()
+    window.destroy()
+            
+def saveinfo():
+    save()
+
+#label and entry box
+label = tk.Label(text="Insert no. of text target")
+entry = tk.Entry()
+
+label.pack()
+entry.pack()
+    
+
+# submit and cancel buttons
+b1 = Button(window, text='Submit',command=saveinfo,width=15,bg='#93c47d',fg='white',font=("times",12,"bold"))
+b1.place(x=85,y=50)
+
+window.mainloop()
